e359f696f8cdc93219588193a9259cac525cf466
Tomas Willaschek <willaschek.t@gmail.com>
2020-11-28 17:23:34 +0100

    added from to request param and graph generator

--- src/generate_graphs.py
+++ src/generate_graphs.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-from src.web.core.session import db_session
 import pandas as pd
 import numpy as np
 
 
-if __name__ == '__main__':
+def generate(_from='2019-01-01', _to='2022-12-30', tmp=False):
     fig_dir = os.path.join(os.path.dirname(__file__), 'web', 'static')
     sql_connector = 'mysql+pymysql://root:root@127.0.0.1/upa'
 
-    q1 = """select
-    c.infected_date,
-    count(*),
-    (select count(*) from covidcase c2 where c2.infected_date <= c.infected_date) as cum_sum
-from covidcase c
-group by c.infected_date
-order by c.infected_date;"""
+    ext = 'tmp_' if tmp else ''
+
+    q1 = f"""select
+        c.infected_date,
+        count(*),
+        (select count(*) from covidcase c2 where c2.infected_date <= c.infected_date) as cum_sum
+    from covidcase c
+    where c.infected_date between DATE("{_from}") and DATE("{_to}")
+    group by c.infected_date
+    order by c.infected_date;"""
 
     fig, ax = plt.subplots()
     data = pd.read_sql_query(q1, 'mysql+pymysql://root:root@127.0.0.1/upa')
@@ -27,23 +29,24 @@ order by c.infected_date;"""
     ax.set_xlabel('')
     # plt.show()
     plt.tight_layout()
-    plt.savefig(os.path.join(fig_dir, 'cumulative_cases.png'))
+    plt.savefig(os.path.join(fig_dir, f'{ext}cumulative_cases.png'))
 
-    q2 = """select 
-    c.infected_date,
-    (select count(*) from covidcase c2 where c2.age between 0 and 10 and c2.infected_date = c.infected_date) as `0-10`,
-    (select count(*) from covidcase c2 where c2.age between 11 and 20 and c2.infected_date = c.infected_date) as `11-20`,
-    (select count(*) from covidcase c2 where c2.age between 21 and 30 and c2.infected_date = c.infected_date) as `21-30`,
-    (select count(*) from covidcase c2 where c2.age between 31 and 40 and c2.infected_date = c.infected_date) as `31-40`,
-    (select count(*) from covidcase c2 where c2.age between 41 and 50 and c2.infected_date = c.infected_date) as `41-50`,
-    (select count(*) from covidcase c2 where c2.age between 51 and 60 and c2.infected_date = c.infected_date) as `51-60`,
-    (select count(*) from covidcase c2 where c2.age between 61 and 70 and c2.infected_date = c.infected_date) as `61-70`,
-    (select count(*) from covidcase c2 where c2.age between 71 and 80 and c2.infected_date = c.infected_date) as `71-80`,
-    (select count(*) from covidcase c2 where c2.age >= 81 and c2.infected_date = c.infected_date) as `81+`,
-    count(*)
-from covidcase c
-group by c.infected_date
-order by c.infected_date;"""
+    q2 = f"""select 
+        c.infected_date,
+        (select count(*) from covidcase c2 where c2.age between 0 and 10 and c2.infected_date = c.infected_date) as `0-10`,
+        (select count(*) from covidcase c2 where c2.age between 11 and 20 and c2.infected_date = c.infected_date) as `11-20`,
+        (select count(*) from covidcase c2 where c2.age between 21 and 30 and c2.infected_date = c.infected_date) as `21-30`,
+        (select count(*) from covidcase c2 where c2.age between 31 and 40 and c2.infected_date = c.infected_date) as `31-40`,
+        (select count(*) from covidcase c2 where c2.age between 41 and 50 and c2.infected_date = c.infected_date) as `41-50`,
+        (select count(*) from covidcase c2 where c2.age between 51 and 60 and c2.infected_date = c.infected_date) as `51-60`,
+        (select count(*) from covidcase c2 where c2.age between 61 and 70 and c2.infected_date = c.infected_date) as `61-70`,
+        (select count(*) from covidcase c2 where c2.age between 71 and 80 and c2.infected_date = c.infected_date) as `71-80`,
+        (select count(*) from covidcase c2 where c2.age >= 81 and c2.infected_date = c.infected_date) as `81+`,
+        count(*)
+    from covidcase c
+    where c.infected_date between "{_from}" and "{_to}"
+    group by c.infected_date
+    order by c.infected_date;"""
 
     labels = ['0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81+']
     fig, ax = plt.subplots()
@@ -55,16 +58,16 @@ order by c.infected_date;"""
     plt.xticks(rotation='vertical')
     # plt.show()
     plt.tight_layout()
-    plt.savefig(os.path.join(fig_dir, 'increase_by_age.png'))
+    plt.savefig(os.path.join(fig_dir, f'{ext}increase_by_age.png'))
 
-    q3 = """select c.infected_date, country.name, count(*) as `total` 
-from covidcase c 
-join country on country.code = c.country_code 
-where country_code not like 'cz' 
-group by c.infected_date, c.country_code 
-having total >= 10
-order by c.infected_date;
-"""
+    q3 = f"""select c.infected_date, country.name, count(*) as `total` 
+    from covidcase c 
+    join country on country.code = c.country_code 
+    where country_code not like 'cz' and c.infected_date between "{_from}" and "{_to}"
+    group by c.infected_date, c.country_code 
+    having total >= 10
+    order by c.infected_date;
+    """
     data = pd.read_sql_query(q3, sql_connector)
     g = sns.relplot(data=data, x='infected_date', y='total', hue='name')
     g.legend.remove()
@@ -74,19 +77,19 @@ order by c.infected_date;
     plt.legend(bbox_to_anchor=(1.0, 1.))
     # plt.show()
     plt.tight_layout()
-    plt.savefig(os.path.join(fig_dir, 'external_country.png'))
+    plt.savefig(os.path.join(fig_dir, f'{ext}external_country.png'))
 
-    q4 = """select
-    (select count(*) from covidcase where age between 0 and 10 and death_date is not null) as `0-10`,
-    (select count(*) from covidcase where age between 11 and 20 and death_date is not null) as `11-20`,
-    (select count(*) from covidcase where age between 21 and 30 and death_date is not null) as `21-30`,
-    (select count(*) from covidcase where age between 31 and 40 and death_date is not null) as `31-40`,
-    (select count(*) from covidcase where age between 41 and 50 and death_date is not null) as `41-50`,
-    (select count(*) from covidcase where age between 51 and 60 and death_date is not null) as `51-60`,
-    (select count(*) from covidcase where age between 61 and 70 and death_date is not null) as `61-70`,
-    (select count(*) from covidcase where age between 71 and 80 and death_date is not null) as `71-80`,
-    (select count(*) from covidcase where age >= 81 and death_date is not null) as `81+`;
-"""
+    q4 = f"""select
+        (select count(*) from covidcase where age between 0 and 10 and death_date is not null and infected_date between "{_from}" and "{_to}") as `0-10`,
+        (select count(*) from covidcase where age between 11 and 20 and death_date is not null and infected_date between "{_from}" and "{_to}") as `11-20`,
+        (select count(*) from covidcase where age between 21 and 30 and death_date is not null and infected_date between "{_from}" and "{_to}") as `21-30`,
+        (select count(*) from covidcase where age between 31 and 40 and death_date is not null and infected_date between "{_from}" and "{_to}") as `31-40`,
+        (select count(*) from covidcase where age between 41 and 50 and death_date is not null and infected_date between "{_from}" and "{_to}") as `41-50`,
+        (select count(*) from covidcase where age between 51 and 60 and death_date is not null and infected_date between "{_from}" and "{_to}") as `51-60`,
+        (select count(*) from covidcase where age between 61 and 70 and death_date is not null and infected_date between "{_from}" and "{_to}") as `61-70`,
+        (select count(*) from covidcase where age between 71 and 80 and death_date is not null and infected_date between "{_from}" and "{_to}") as `71-80`,
+        (select count(*) from covidcase where age >= 81 and death_date is not null and infected_date between "{_from}" and "{_to}") as `81+`;
+    """
 
     fig, ax = plt.subplots()
     data = pd.read_sql_query(q4, sql_connector)
@@ -96,18 +99,18 @@ order by c.infected_date;
     ax.set_yticklabels(labels)
     # plt.show()
     plt.tight_layout()
-    plt.savefig(os.path.join(fig_dir, 'mortality_by_age.png'))
+    plt.savefig(os.path.join(fig_dir, f'{ext}mortality_by_age.png'))
 
-
-    q5 = """select
-    c.infected_date as `infected_date`,
-    (select count(*) from covidcase where infected_date = c.infected_date and gender = 'm' and death_date is not null) as `muzi`,
-    (select count(*) from covidcase where infected_date = c.infected_date and gender = 'f' and death_date is not null) as `zeny`,
-    count(*)
-    from covidcase c
-    group by c.infected_date
-    order by c.infected_date;
-    """
+    q5 = f"""select
+        c.infected_date as `infected_date`,
+        (select count(*) from covidcase where infected_date = c.infected_date and gender = 'm' and death_date is not null) as `muzi`,
+        (select count(*) from covidcase where infected_date = c.infected_date and gender = 'f' and death_date is not null) as `zeny`,
+        count(*)
+        from covidcase c
+        where c.infected_date between "{_from}" and "{_to}"
+        group by c.infected_date
+        order by c.infected_date;
+        """
 
     fig, ax = plt.subplots()
     data = pd.read_sql_query(q5, sql_connector)
@@ -117,5 +120,8 @@ order by c.infected_date;
     plt.legend(loc='best')
     plt.tight_layout()
     # plt.show()
-    plt.savefig(os.path.join(fig_dir, 'mortality_by_sex.png'))
+    plt.savefig(os.path.join(fig_dir, f'{ext}mortality_by_sex.png'))
 
+
+if __name__ == '__main__':
+    generate()
--- src/web/views/base.py
+++ src/web/views/base.py
@@ -1,19 +1,19 @@
 
-from flask import render_template
+from flask import render_template, request
 from flask.views import MethodView
 from .second_query_map import get_map
+from generate_graphs import generate
 
 
 class Base(MethodView):
     def get(self):
-        return render_template('base.html')
-
-
-class SecondQuery(MethodView):
-    def get(self):
-        return render_template("second_query.html", map_data=get_map())
+        tmp = False
+        if request.args:
+            args = request.args
+            generate(args['from'], args['to'], True)
+            tmp = True
+        return render_template('base.html', map_data=get_map(), tmp='tmp_' if tmp else '')
 
 
 def configure(app):
     app.add_url_rule('/', view_func=Base.as_view('index'))
-    app.add_url_rule('/secondquery', view_func=SecondQuery.as_view('secondquery'))
